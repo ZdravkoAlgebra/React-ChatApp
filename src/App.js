e41bf97964fde4390aa113110f1a974353d4298c
ZdravkoAlgebra <118934071+ZdravkoAlgebra@users.noreply.github.com>
2023-04-12 21:38:07 +0200

    app

--- src/App.js
+++ src/App.js
@@ -1,102 +1,78 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 import Messages from "./components/Messages/Messages";
 import Input from "./components/Input/Input";
 import { randomColor, randomName } from "./utils/style/data";
-import { AppHeader, AppWrapper,GlobalStyles } from './utils/style/defaultStyles';
-
+import {
+  AppHeader,
+  AppWrapper,
+  GlobalStyles,
+} from "./utils/style/defaultStyles";
 function App() {
   const [member, setMember] = useState({
     username: randomName(),
     color: randomColor(),
   });
   const [drone, setDrone] = useState(null);
-
   const [messages, setMessages] = useState([]);
-
+  const [isDroneSet, setIsDroneSet] = useState(false);
   useEffect(() => {
-    console.log("useEffect");
     const drone = new window.Scaledrone("7IJPlwfF3zL37kgK", {
-      data: member
+      data: member,
     });
     setDrone(drone);
-  }, [member]);
-
+  }, []);
   useEffect(() => {
-    console.log("useEffect ");
-    // DEFAULT SCALEDRONE ACTIONS
-    const droneEvents = () => {
-      drone.on("open", (error) => {
+    if (drone) {
+      drone.on("open", error => {
         if (error) {
           return console.error(error);
         }
-        roomEvents();
+        setMember(member => ({ ...member, id: drone.clientId }));
       });
-      drone.on("error", (error) => console.error(error));
+      drone.on("error", error => console.error(error));
       drone.on("disconnect", () => {
-        console.log(
-          "Disconnected, Scaledrone reconnect"
-        );
+        console.log("Disconnected, Scaledrone reconnect");
       });
       drone.on("reconnect", () => {
         console.log("Reconnected");
       });
-    };
-
-    // SCALEDRONE HAS ROOMS WHICH HAVE SOME ACTIONS
-    const roomEvents = () => {
+      setIsDroneSet(true);
+    }
+  }, [drone]);
+  useEffect(() => {
+    if (drone) {
       const room = drone.subscribe(`observable-room`);
-      room.on("open", (error) => {
+      room.on("open", error => {
         if (error) {
           console.error(error);
         } else {
           console.log("Im here in room");
         }
       });
-      room.on("message", (message) => {
-        receiveMsg(message);
-      });
-    };
-
-    // RECEIVING MESSAGES FROM SCALEDRONE
-    const receiveMsg = (message) => {
-      console.log("Received message:", message);
-      setMessages(messages =>[...messages,message]);
-    };
-
-    if (drone && !member.username) {
-      droneEvents();
-    }
-  }, [drone, member]);
-
-  const onSendMessage = (message) => {
-    console.log("Sending message:", message);
-    if (drone && drone.id) {
-      drone.publish({
-        room: "observable-room",
-        text: message,
-        member,
+      room.on("data", (data, member) => {
+        setMessages(message => [...message, { member: member, text: data }]);
       });
     }
+  }, [isDroneSet, messages]);
+  const onSendMessage = message => {
+    drone.publish({
+      room: "observable-room",
+      message: message,
+    });
+    console.log(messages);
   };
-
   return (
     <>
-    <GlobalStyles />
-    <AppWrapper>
-      <AppHeader>
-        <h1>My Chat App</h1>
-      </AppHeader>
-      <Messages
-        messages={messages}
-        currentMember={member}
-      />
-      <Input
-        onSendMessage={onSendMessage}
-      />
-    </AppWrapper>
+      <GlobalStyles />
+      <AppWrapper>
+        <AppHeader>
+          <h1>My Chat App</h1>
+        </AppHeader>
+        <Messages messages={messages} currentMember={member} />
+        <Input onSendMessage={onSendMessage} />
+      </AppWrapper>
     </>
   );
 }
-
 export default App;
